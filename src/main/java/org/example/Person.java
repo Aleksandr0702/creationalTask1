1c3534988a448463e54e38bfc4a10f4e7eda9141
Aleksandr0702 <110557601+Aleksandr0702@users.noreply.github.com>
2023-11-23 23:21:09 +0600

    first commit

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,62 @@
+package org.example;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    protected int age;
+    protected String address;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public boolean hasAge() {
+        return age > 0;
+    }
+
+    public boolean hasAddress() {
+        return address != null && !address.isEmpty();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void happyBirthday() {
+        age++;
+    }
+
+    public PersonBuilder newChildBuilder() {
+        PersonBuilder builder = new PersonBuilder();
+        builder.setSurname(this.surname);
+        return builder;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + surname + ", age " + age + ", living in " + (address != null ? address : "unknown");
+    }
+}
